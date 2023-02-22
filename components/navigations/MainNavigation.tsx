b2d389051324d328a43b83d9dd5c8b6c26d36eed
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2023-02-23 00:15:48 +0100

    fix:updated navigations types and components functionality

--- components/navigations/MainNavigation.tsx
+++ components/navigations/MainNavigation.tsx
@@ -1,17 +1,20 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { DASHBOARD, SETTINGS } from 'libs/constants'
-import { TMainNavigation } from 'libs/types'
+import { MainNavigationScreens } from 'libs/types'
 import React from 'react'
 
 import { Settings } from 'components/screens'
 
 import { Dashboard } from './Dashboard'
 
-const Stack = createNativeStackNavigator<TMainNavigation>()
+const Stack = createNativeStackNavigator<MainNavigationScreens>()
 
 export const MainNavigation = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
       <Stack.Screen name={DASHBOARD} component={Dashboard} />
       <Stack.Screen name={SETTINGS} component={Settings} />
     </Stack.Navigator>
