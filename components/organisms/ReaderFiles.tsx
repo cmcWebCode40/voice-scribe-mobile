d77aca72b2c5dee68f6d98ed5aa041c2f7e73ef2
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2023-02-28 19:12:06 +0100

    feat: added expo speech cutom hooks

--- components/organisms/ReaderFiles.tsx
+++ components/organisms/ReaderFiles.tsx
@@ -1,5 +1,8 @@
-import { ReaderFileInformation } from 'libs/types'
-import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { WORD_READER } from 'libs/constants'
+import { MainNavigationScreens, ReaderFileInformation } from 'libs/types'
+import React, { useCallback } from 'react'
 import { FlatList, StyleSheet, View } from 'react-native'
 
 import { ReaderFileItem } from 'components/molecules'
@@ -10,8 +13,18 @@ type ReaderFilesProps = {
 export const ReaderFiles: React.FunctionComponent<ReaderFilesProps> = ({
   files,
 }) => {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<MainNavigationScreens>>()
+
+  const navigateToWordReaderScreen = useCallback(() => {
+    navigation.navigate(WORD_READER)
+  }, [navigation])
   const Item = (information: ReaderFileInformation) => (
-    <ReaderFileItem style={styles.fileItem} fileInformation={information} />
+    <ReaderFileItem
+      style={styles.fileItem}
+      fileInformation={information}
+      onPress={navigateToWordReaderScreen}
+    />
   )
 
   if (files.length < 0) {
